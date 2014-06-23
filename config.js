01f1718aead7301a2c52a21e9598809058ef2720
Ryex <ryexander@gmail.com>
2014-06-22 21:01:23 -0600

    changing how the config works

--- config.js
+++ config.js
@@ -2,12 +2,12 @@
 
 module.exports = {
 
-    serverIp: '0.0.0.0',
+    serverIp: process.env.HOST || '0.0.0.0',
     serverPort: process.env.PORT || 3000,
 
-    dbName: 'btcebot',
-    dbHost: '127.0.0.1',
-    dbPort: 27017,
+    dbName: process.env.DB_NAME || 'rybot',
+    dbHost: process.env.DB_HOST || '127.0.0.1',
+    dbPort: process.env.DB_PORT || 27017,
 
     logDays: 30,
 
